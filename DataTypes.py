1801b6a760411be5864b7eb60cf84d9a3b8e2d08
Umer Nadir <nadirhameed0@gmail.com>
2026-02-20 17:06:37 +0300

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,21 @@
+name = "Umer"
+age = 15
+is_student = True
+weight = 55.5
+
+print("Name :", name)
+print("Data Type of name is :", type(name))
+print("Age :", age)
+print("Data Type of Age is :", type(age))
+print("Is_student :", is_student)
+print("Data Type of is_student is :", type(is_student))
+print("weight :", weight)
+print("Data type of weight is :", type(weight))
+
+print("\n after type casting....")
+age = str(age)
+print(age)
+print("Data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data type of weight is", type(weight))
