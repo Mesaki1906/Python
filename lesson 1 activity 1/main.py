7a64f060a9072aa0a355fcdda7cde63303693396
Mesaki1906 <nadirhameed0@gmail.com>
2026-09-01 16:06:49 +0300

    Inital Commit

---
+++ lesson 1 activity 1/main.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello world!")
+
+# Printing my name
+print("My Name is Umer")
+
+# Printing a funfact abour myself
+print("I Like to play chess and football")
+
+# Performing and Printing simple calculation
+print("The sum of 6 and 7 is;", 6 + 7)
