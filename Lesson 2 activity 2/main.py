77261b0f8d70d94d82ff58942c46f8a64bd58b17
Nadir Hameed <nadirhameed0@gmail.com>
2026-09-03 16:19:13 +0300

    Add files via upload

---
+++ Lesson 2 activity 2/main.py
@@ -0,0 +1,13 @@
+# Creating variables
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+code = "codingal"
+print(code)
+
+name = input("Enter your name: ")
+
+print("\nHello", name, "\nWelcome to codingal")
