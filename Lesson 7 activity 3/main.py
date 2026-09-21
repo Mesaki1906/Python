1cf0b3c51962f64200e367a0bb67343f8612f8f8
Nadir Hameed <nadirhameed0@gmail.com>
2026-09-21 15:59:24 +0300

    Create main.py

---
+++ Lesson 7 activity 3/main.py
@@ -0,0 +1,28 @@
+print("Enter marks obtained in 5 subjects")
+
+markOne = int(input())
+markTwo = int(input())
+markThree = int(input())
+markFour = int(input())
+markFive = int(input())
+
+tot = markOne + markTwo + markThree + markFour + markFive
+avg = int(tot / 5)
+validRange = range (0, 101)
+
+if avg not in validRange:
+    print("INVALID INPUT!")
+elif avg in range (91,101):
+    print("Your Grade is A1")
+elif avg in range (81,91):
+    print("Your Grade is A2")
+elif avg in range (71, 81):
+    print("Your Grade is B1")
+elif avg in range (61, 71):
+    print("Your Grade is B2")
+elif avg in range (51, 61):
+    print("Your Grade is C1")
+elif avg in range (41, 51):
+    print("Your Grade is C2")
+elif avg in range (0, 41):
+    print("Your Grade is F")
